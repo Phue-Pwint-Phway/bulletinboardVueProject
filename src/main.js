9011f00aee3fb10975d16feed5afce5726815f03
Phue-Pwint-Phway <scm.phuepwintphway@gmail.com>
2021-03-09 14:15:34 +0630

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap';
-// import  './assets/app.scss';
+import router from './router'
+import './axios'
+import store from './vuex'
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
+  
   render: h => h(App),
 }).$mount('#app')
